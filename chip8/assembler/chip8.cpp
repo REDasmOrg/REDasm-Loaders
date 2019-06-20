e4ac74e2c9700f5dfdcc78bc08e2916a134c42e3
Dax <trogu.davide@gmail.com>
2019-06-20 21:38:14 +0200

    Re-enabled CHIP-8 plugin

--- chip8/assembler/chip8.cpp
+++ chip8/assembler/chip8.cpp
@@ -1,7 +1,7 @@
 #include "chip8.h"
 #include "chip8_printer.h"
 
-#define SET_DECODE_TO(opmask, cb) m_opcodes[opmask] = [this](u16 opcode, const InstructionPtr& instruction) -> bool { return cb(opcode, instruction); };
+#define SET_DECODE_TO(opmask, cb) m_opcodes[opmask] = [this](u16 opcode, Instruction* instruction) -> bool { return cb(opcode, instruction); };
 
 Chip8Assembler::Chip8Assembler(): Assembler()
 {
@@ -26,11 +26,11 @@ Chip8Assembler::Chip8Assembler(): Assembler()
 size_t Chip8Assembler::bits() const { return 16; }
 Printer *Chip8Assembler::doCreatePrinter(Disassembler *disassembler) const { return new Chip8Printer(disassembler); }
 
-bool Chip8Assembler::decodeInstruction(const BufferView& view, const InstructionPtr &instruction)
+bool Chip8Assembler::decodeInstruction(const BufferView& view, Instruction *instruction)
 {
     u16be opcode = static_cast<u16be>(view);
-    instruction->id = opcode;
-    instruction->size = sizeof(u16);
+    instruction->setId(opcode);
+    instruction->setSize(sizeof(u16));
 
     auto it = m_opcodes.find(opcode & 0xF000);
 
@@ -40,146 +40,146 @@ bool Chip8Assembler::decodeInstruction(const BufferView& view, const Instruction
     return true;
 }
 
-void Chip8Assembler::onDecoded(const InstructionPtr &instruction)
+void Chip8Assembler::onDecoded(Instruction *instruction)
 {
-    if(instruction->mnemonic == "rts")
-        instruction->type = InstructionType::Stop;
-    else if(instruction->mnemonic == "jmp")
-        instruction->type = InstructionType::Jump;
-    else if((instruction->mnemonic == "ske") || (instruction->mnemonic == "skne") || (instruction->mnemonic == "skp") || (instruction->mnemonic == "sknp"))
-        instruction->type = InstructionType::ConditionalJump;
-    else if(instruction->mnemonic == "call")
-        instruction->type = InstructionType::Call;
-    else if(instruction->mnemonic == "add")
-        instruction->type = InstructionType::Add;
-    else if(instruction->mnemonic == "sub")
-        instruction->type = InstructionType::Sub;
-    else if(instruction->mnemonic == "and")
-        instruction->type = InstructionType::And;
-    else if(instruction->mnemonic == "or")
-        instruction->type = InstructionType::Or;
-    else if(instruction->mnemonic == "xor")
-        instruction->type = InstructionType::Xor;
-    else if((instruction->mnemonic == "mov") || (instruction->mnemonic == "ldra"))
-        instruction->type = InstructionType::Load;
-    else if(instruction->mnemonic == "stra")
-        instruction->type = InstructionType::Store;
-    else if(instruction->mnemonic == "sys")
-        instruction->type = InstructionType::Privileged;
+    if(instruction->is("rts"))
+        instruction->setType(InstructionType::Stop);
+    else if(instruction->is("jmp"))
+        instruction->setType(InstructionType::Jump);
+    else if((instruction->is("ske") || instruction->is("skne") || instruction->is("skp") || instruction->is("sknp")))
+        instruction->setType(InstructionType::ConditionalJump);
+    else if(instruction->is("call"))
+        instruction->setType(InstructionType::Call);
+    else if(instruction->is("add"))
+        instruction->setType(InstructionType::Add);
+    else if(instruction->is("sub"))
+        instruction->setType(InstructionType::Sub);
+    else if(instruction->is("and"))
+        instruction->setType(InstructionType::And);
+    else if(instruction->is( "or"))
+        instruction->setType(InstructionType::Or);
+    else if(instruction->is("xor"))
+        instruction->setType(InstructionType::Xor);
+    else if(instruction->is("mov") || instruction->is("ldra"))
+        instruction->setType(InstructionType::Load);
+    else if(instruction->is("stra"))
+        instruction->setType(InstructionType::Store);
+    else if(instruction->is("sys"))
+        instruction->setType(InstructionType::Privileged);
 }
 
-bool Chip8Assembler::decode0xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode0xxx(u16 opcode, Instruction *instruction) const
 {
     if(opcode == 0x00E0)
-        instruction->mnemonic = "cls";
+        instruction->setMnemonic("cls");
     else if(opcode == 0x00EE)
-        instruction->mnemonic = "rts";
+        instruction->setMnemonic("rts");
     else if(opcode == 0x00FB) // SuperChip only
-        instruction->mnemonic = "scright";
+        instruction->setMnemonic("scright");
     else if(opcode == 0x00FC) // SuperChip only
-        instruction->mnemonic = "scleft";
+        instruction->setMnemonic("scleft");
     else if(opcode == 0x00FE) // SuperChip only
-        instruction->mnemonic = "low";
+        instruction->setMnemonic("low");
     else if(opcode == 0x00FF) // SuperChip only
-        instruction->mnemonic = "high";
+        instruction->setMnemonic("high");
     else if((opcode & 0x00F0) == 0x00C0) // SuperChip only
     {
-        instruction->mnemonic = "scdown";
+        instruction->setMnemonic("scdown");
         instruction->cnst(opcode & 0x000F);
     }
     else
     {
-        instruction->mnemonic = "sys";
+        instruction->setMnemonic("sys");
         instruction->cnst(opcode & 0x0FFF);
     }
 
     return true;
 }
 
-bool Chip8Assembler::decode1xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode1xxx(u16 opcode, Instruction *instruction) const
 {
-    instruction->mnemonic = "jmp";
+    instruction->setMnemonic("jmp");
     instruction->imm(opcode & 0x0FFF);
     instruction->targetIdx(0);
     return true;
 }
 
-bool Chip8Assembler::decode2xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode2xxx(u16 opcode, Instruction *instruction) const
 {
-    instruction->mnemonic = "call";
+    instruction->setMnemonic("call");
     instruction->imm(opcode & 0x0FFF);
     instruction->targetIdx(0);
     return true;
 }
 
-bool Chip8Assembler::decode3xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode3xxx(u16 opcode, Instruction *instruction) const
 {
-    instruction->mnemonic = "ske";
+    instruction->setMnemonic("ske");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->imm(opcode & 0x00FF);
-    instruction->target(instruction->endAddress() + instruction->size);
+    instruction->target(instruction->endAddress() + instruction->size());
     return true;
 }
 
-bool Chip8Assembler::decode4xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode4xxx(u16 opcode, Instruction *instruction) const
 {
-    instruction->mnemonic = "skne";
+    instruction->setMnemonic("skne");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->imm(opcode & 0x00FF);
-    instruction->target(instruction->endAddress() + instruction->size);
+    instruction->target(instruction->endAddress() + instruction->size());
     return true;
 }
 
-bool Chip8Assembler::decode5xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode5xxx(u16 opcode, Instruction *instruction) const
 {
     if((opcode & 0x000F) != 0)
         return false;
 
-    instruction->mnemonic = "ske";
+    instruction->setMnemonic("ske");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->reg((opcode & 0x00F0) >> 4);
-    instruction->target(instruction->endAddress() + instruction->size);
+    instruction->target(instruction->endAddress() + instruction->size());
     return true;
 }
 
-bool Chip8Assembler::decode6xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode6xxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "mov";
+    instruction->setMnemonic("mov");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->cnst(opcode & 0x00FF);
     return true;
 }
 
-bool Chip8Assembler::decode7xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode7xxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "add";
+    instruction->setMnemonic("add");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->cnst(opcode & 0x00FF);
     return true;
 }
 
-bool Chip8Assembler::decode8xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode8xxx(u16 opcode, Instruction* instruction) const
 {
     u8 op = opcode & 0x000F;
 
     if(op == 0x0)
-        instruction->mnemonic = "mov";
+        instruction->setMnemonic("mov");
     else if(op == 0x1)
-        instruction->mnemonic = "or";
+        instruction->setMnemonic("or");
     else if(op == 0x2)
-        instruction->mnemonic = "and";
+        instruction->setMnemonic("and");
     else if(op == 0x3)
-        instruction->mnemonic = "xor";
+        instruction->setMnemonic("xor");
     else if(op == 0x4)
-        instruction->mnemonic = "add";
+        instruction->setMnemonic("add");
     else if(op == 0x5)
-        instruction->mnemonic = "sub";
+        instruction->setMnemonic("sub");
     else if(op == 0x6)
-        instruction->mnemonic = "shr";
+        instruction->setMnemonic("shr");
     else if(op == 0x7)
-        instruction->mnemonic = "sub";
+        instruction->setMnemonic("sub");
     else if(op == 0xE)
-        instruction->mnemonic = "shl";
+        instruction->setMnemonic("shl");
     else
         return false;
 
@@ -191,91 +191,91 @@ bool Chip8Assembler::decode8xxx(u16 opcode, const InstructionPtr &instruction) c
     return true;
 }
 
-bool Chip8Assembler::decode9xxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decode9xxx(u16 opcode, Instruction* instruction) const
 {
     if((opcode & 0x000F) != 0)
         return false;
 
-    instruction->mnemonic = "skne";
+    instruction->setMnemonic("skne");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->reg((opcode & 0x00F0) >> 4);
-    instruction->target(instruction->endAddress() + instruction->size);
+    instruction->target(instruction->endAddress() + instruction->size());
     return true;
 }
 
-bool Chip8Assembler::decodeAxxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeAxxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "mov";
+    instruction->setMnemonic("mov");
     instruction->reg(CHIP8_REG_I_ID, CHIP8_REG_I);
     instruction->cnst(opcode & 0x0FFF);
     return true;
 }
 
-bool Chip8Assembler::decodeBxxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeBxxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "jmp";
+    instruction->setMnemonic("jmp");
     instruction->disp(CHIP8_REG_V0_ID, opcode & 0x0FFF);
     return true;
 }
 
-bool Chip8Assembler::decodeCxxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeCxxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "rand";
+    instruction->setMnemonic("rand");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->cnst(opcode & 0x00FF);
     return true;
 }
 
-bool Chip8Assembler::decodeDxxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeDxxx(u16 opcode, Instruction* instruction) const
 {
-    instruction->mnemonic = "draw";
+    instruction->setMnemonic("draw");
     instruction->reg((opcode & 0x0F00) >> 8);
     instruction->reg((opcode & 0x00F0) >> 4);
     instruction->cnst(opcode & 0x000F);
     return true;
 }
 
-bool Chip8Assembler::decodeExxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeExxx(u16 opcode, Instruction* instruction) const
 {
     u16 op = opcode & 0xFF;
 
     if(op == 0x9E)
-        instruction->mnemonic = "skp";
+        instruction->setMnemonic("skp");
     else if(op == 0xA1)
-        instruction->mnemonic = "sknp";
+        instruction->setMnemonic("sknp");
 
     instruction->reg((opcode & 0x0F00) >> 8, CHIP8_REG_K);
-    instruction->target(instruction->endAddress() + instruction->size);
+    instruction->target(instruction->endAddress() + instruction->size());
     return true;
 }
 
-bool Chip8Assembler::decodeFxxx(u16 opcode, const InstructionPtr &instruction) const
+bool Chip8Assembler::decodeFxxx(u16 opcode, Instruction* instruction) const
 {
     u16 op = opcode & 0x00FF;
 
     if(op == 0x07)
-        instruction->mnemonic = "gdelay";
+        instruction->setMnemonic("gdelay");
     else if(op == 0x0A)
-        instruction->mnemonic = "wkey";
+        instruction->setMnemonic("wkey");
     else if(op == 0x15)
-        instruction->mnemonic = "sdelay";
+        instruction->setMnemonic("sdelay");
     else if(op == 0x18)
-        instruction->mnemonic = "ssound";
+        instruction->setMnemonic("ssound");
     else if(op == 0x1E)
     {
-        instruction->mnemonic = "add";
+        instruction->setMnemonic("add");
         instruction->reg(CHIP8_REG_I_ID, CHIP8_REG_I);
     }
     else if(op == 0x29)
-        instruction->mnemonic = "font";
+        instruction->setMnemonic("font");
     else if(op == 0x30) // SuperChip only
-        instruction->mnemonic = "xfont";
+        instruction->setMnemonic("xfont");
     else if(op == 0x33)
-        instruction->mnemonic = "bcd";
+        instruction->setMnemonic("bcd");
     else if(op == 0x55)
-        instruction->mnemonic = "stra";
+        instruction->setMnemonic("stra");
     else if(op == 0x65)
-        instruction->mnemonic = "ldra";
+        instruction->setMnemonic("ldra");
     else
         return false;
 
--- chip8/assembler/chip8.h
+++ chip8/assembler/chip8.h
@@ -16,7 +16,7 @@ using namespace REDasm;
 class Chip8Assembler : public Assembler
 {
     private:
-        typedef std::function<bool(u16, const InstructionPtr& instruction)> OpCodeCallback;
+        typedef std::function<bool(u16, Instruction*)> OpCodeCallback;
 
     public:
         Chip8Assembler();
@@ -24,26 +24,26 @@ class Chip8Assembler : public Assembler
 
     protected:
         Printer* doCreatePrinter(Disassembler *disassembler) const override;
-        bool decodeInstruction(const BufferView &view, const InstructionPtr& instruction) override;
-        void onDecoded(const InstructionPtr& instruction) override;
+        bool decodeInstruction(const BufferView &view, Instruction* instruction) override;
+        void onDecoded(Instruction* instruction) override;
 
     private:
-        bool decode0xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode1xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode2xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode3xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode4xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode5xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode6xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode7xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode8xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decode9xxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeAxxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeBxxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeCxxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeDxxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeExxx(u16 opcode, const InstructionPtr& instruction) const;
-        bool decodeFxxx(u16 opcode, const InstructionPtr& instruction) const;
+        bool decode0xxx(u16 opcode, Instruction* instruction) const;
+        bool decode1xxx(u16 opcode, Instruction* instruction) const;
+        bool decode2xxx(u16 opcode, Instruction* instruction) const;
+        bool decode3xxx(u16 opcode, Instruction* instruction) const;
+        bool decode4xxx(u16 opcode, Instruction* instruction) const;
+        bool decode5xxx(u16 opcode, Instruction* instruction) const;
+        bool decode6xxx(u16 opcode, Instruction* instruction) const;
+        bool decode7xxx(u16 opcode, Instruction* instruction) const;
+        bool decode8xxx(u16 opcode, Instruction* instruction) const;
+        bool decode9xxx(u16 opcode, Instruction* instruction) const;
+        bool decodeAxxx(u16 opcode, Instruction* instruction) const;
+        bool decodeBxxx(u16 opcode, Instruction* instruction) const;
+        bool decodeCxxx(u16 opcode, Instruction* instruction) const;
+        bool decodeDxxx(u16 opcode, Instruction* instruction) const;
+        bool decodeExxx(u16 opcode, Instruction* instruction) const;
+        bool decodeFxxx(u16 opcode, Instruction* instruction) const;
 
     private:
         std::unordered_map<u16, OpCodeCallback> m_opcodes;
