b48a825d93b624be77c0842756390063e6195ee9
Dax <contact@antoniodavide.dev>
2020-05-09 21:14:11 +0200

    PSX-EXE: Sync code to API

--- psxexe/psxexe.cpp
+++ psxexe/psxexe.cpp
@@ -19,12 +19,12 @@ void PsxExeLoader::load(RDLoaderPlugin*, RDLoader* loader)
     const auto* header = reinterpret_cast<const PsxExeHeader*>(RDLoader_GetData(loader));
 
     if(header->t_addr > PSX_USER_RAM_START)
-        RDDocument_AddSegment(doc, "RAM0", 0, PSX_USER_RAM_START, (header->t_addr - PSX_USER_RAM_START), SegmentType_Bss);
+        RDDocument_AddSegment(doc, "RAM0", 0, PSX_USER_RAM_START, (header->t_addr - PSX_USER_RAM_START), SegmentFlags_Bss);
 
-    RDDocument_AddSegment(doc, "TEXT", PSXEXE_TEXT_OFFSET, header->t_addr, header->t_size, SegmentType_CodeData);
+    RDDocument_AddSegment(doc, "TEXT", PSXEXE_TEXT_OFFSET, header->t_addr, header->t_size, SegmentFlags_CodeData);
 
     if((header->t_addr + header->t_size) < PSX_USER_RAM_END)
-        RDDocument_AddSegment(doc, "RAM1", 0, header->t_addr + header->t_size, PSX_USER_RAM_END - (header->t_addr + header->t_size), SegmentType_Bss);
+        RDDocument_AddSegment(doc, "RAM1", 0, header->t_addr + header->t_size, PSX_USER_RAM_END - (header->t_addr + header->t_size), SegmentFlags_Bss);
 
     RDDocument_SetEntry(doc, header->pc0);
 }
