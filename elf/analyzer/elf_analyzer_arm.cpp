47c2f3146ccfc1d920f6beada959155b2157a995
Dax <contact@antoniodavide.dev>
2022-04-03 10:56:30 +0200

    Sync to ARM Lifter changes

--- elf/analyzer/elf_analyzer_arm.cpp
+++ elf/analyzer/elf_analyzer_arm.cpp
@@ -1,6 +1,5 @@
 #include "elf_analyzer_arm.h"
 #include "../elf.h"
-#include <cstring>
 
 ElfAnalyzer_ARM::ElfAnalyzer_ARM(RDContext* ctx): ElfAnalyzer(ctx) { }
 
@@ -26,12 +25,12 @@ void ElfAnalyzer_ARM::checkPLT32(RDDocument* doc, rd_address funcaddress)
 {
     rd_ptr<RDILFunction> il(RDILFunction_Create(m_context, funcaddress));
 
-    if(!RDILFunction_Match(il.get(), "reg = reg + cnst;"
+    if(!RDILFunction_Match(il.get(), "reg = cnst;"
                                      "reg = reg + cnst;"
                                      "reg = [reg + cnst]")) return;
 
     //TODO: Improve matching algorithm
-    /* if(!RDILFunction_Match(il.get(), "$ip = $pc + cnst;"
+    /* if(!RDILFunction_Match(il.get(), "$ip = cnst;"
                                         "$ip = $ip + cnst;"
                                         "$pc = [$ip + cnst]")) return; */
 
@@ -40,16 +39,11 @@ void ElfAnalyzer_ARM::checkPLT32(RDDocument* doc, rd_address funcaddress)
     size_t n = RDILFunction_Extract(il.get(), &values);
 
     if(!n ||
-       std::strcmp(values[0].reg, values[3].reg) ||
-       std::strcmp(values[0].reg, values[4].reg) ||
-       std::strcmp(values[0].reg, values[7].reg) ||
-       std::strcmp(values[1].reg, values[6].reg) ||
-       std::strcmp(values[0].reg, "ip")          ||
-       std::strcmp(values[1].reg, "pc"))
+       !RD_StrEquals(values[0].reg, values[2].reg) || !RD_StrEquals(values[0].reg, values[3].reg) || !RD_StrEquals(values[0].reg, values[6].reg) ||
+       !RD_StrEquals(values[0].reg, "ip")          || !RD_StrEquals(values[5].reg, "pc"))
         return;
 
-    rd_address address = funcaddress + (sizeof(u32) * 2) +
-                         values[2].u_value + values[5].u_value + values[8].u_value;
+    rd_address address = values[1].u_value + values[4].u_value + values[7].u_value;
 
     rd_flag flags = RDDocument_GetFlags(doc, address);
 
